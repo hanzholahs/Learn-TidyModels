45a13e2dce7070aafa2ae4ef5737ab0ec3b96fe2
hanzholahs <hanzholahs@gmail.com>
2023-09-12 14:58:01 +0700

    learn ch 8-9

--- 01-basic.R
+++ 01-basic.R
@@ -126,3 +126,81 @@ final_lm_res
 extract_workflow(final_lm_res)
 collect_metrics(final_lm_res)
 collect_predictions(final_lm_res)
+
+
+# Feature engineering -----------------------------------------------------
+
+simple_ames <- recipe(Sale_Price ~ Neighborhood + Gr_Liv_Area + Year_Built +
+                        Bldg_Type + Latitude + Longitude, data = ames_train) |> 
+  step_log(Gr_Liv_Area, base = 10) |> 
+  step_other(Neighborhood, threshold = 0.01) |> 
+  step_dummy(all_nominal_predictors()) |> 
+  step_interact(~ Gr_Liv_Area:starts_with("Bldg_Type_")) |> 
+  step_ns(c(Latitude, Longitude), deg_free = 20) |> 
+  step_normalize(matches("(_SF$|^Gr_Liv_)")) |> 
+  step_pca(matches("(_SF$|^Gr_Liv_)"))
+
+stringr::str_subset(names(ames), "(_SF$|^Gr_Liv_)")
+simple_ames
+tidy(simple_ames)
+
+lm_wfl <- workflow() |> 
+  add_model(lm_mdl) |> 
+  add_recipe(simple_ames)
+lm_wfl
+
+lm_fit <- fit(lm_wfl, ames_train)
+predict(lm_fit, ames_test %>% slice(1:3))
+
+extract_fit_engine(lm_fit) |> tidy()
+extract_fit_parsnip(lm_fit)
+extract_recipe(lm_fit, estimated = TRUE) |> tidy()
+
+
+# Model evaluation --------------------------------------------------------
+
+ames_test_res <- predict(lm_fit, ames_test) |> 
+  bind_cols(ames_test |> select(Sale_Price))
+ames_test_res
+
+ggplot(ames_test_res, aes(x = Sale_Price, y = .pred)) +
+  geom_point(alpha = .3) +
+  geom_abline(lty = 2) +
+  coord_obs_pred()
+
+rmse(ames_test_res, truth = Sale_Price, estimate = .pred)
+
+ames_metrics <- metric_set(rmse, mae, rsq)
+ames_metrics(ames_test_res, truth = Sale_Price, estimate = .pred)
+
+# classification settings
+data("two_class_example")
+two_class_example
+
+conf_mat(two_class_example, truth = truth, estimate = predicted)
+accuracy(two_class_example, truth = truth, estimate = predicted)
+mcc(two_class_example, truth = truth, estimate = predicted)
+f_meas(two_class_example, truth = truth, estimate = predicted)
+
+classification_metrics <- metric_set(accuracy, mcc, f_meas)
+classification_metrics(two_class_example, truth = truth, estimate = predicted)
+
+classification_curve <- roc_curve(two_class_example, truth, Class1)
+roc_auc(classification_curve)
+
+autoplot(classification_curve)
+
+f_meas(hpc_cv, truth = obs, estimate = pred, estimator = "macro_weighted")
+classification_metrics(hpc_cv, truth = obs, estimate = pred, estimator = "macro")
+classification_metrics(hpc_cv, truth = obs, estimate = pred, estimator = "micro")
+
+roc_auc(hpc_cv, obs, c("VF", "F", "M", "L"))
+
+hpc_cv |> 
+  group_by(Resample) |> 
+  accuracy(obs, pred)
+
+hpc_cv %>% 
+  group_by(Resample) %>% 
+  roc_curve(obs, VF, F, M, L) %>% 
+  autoplot()
